ae29b21586f6026a693dc9392518743c0f5eed9f
Michelle S Bachler <michelle.bachler@open.ac.uk>
2022-06-28 11:08:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# isws2022-inrupt
+ inrupt code for the ISWS 2022 demonstrator 
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+module.exports = {
+   mode: "development",
+   entry: "./src/inruptlib.js",
+   output: {
+     path: path.resolve(__dirname, "dist"),
+     filename: "inruptlib.js",
+     libraryTarget: 'var',
+     library: 'Inrupt'
+   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ { loader: "style-loader" }, { loader: "css-loader" } ],
+      },
+    ]
+  },
+  devServer: {
+    static: "./lib"
+  }
+};
